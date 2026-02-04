42542ab69f46e39d4f8f2aa2a39386425a3326b9
ajagatobby <150749949+ajagatobby@users.noreply.github.com>
2026-02-04 07:07:22 +0100

    Initial Commit

---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Abdulbasit Ajaga on 04/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
